acdbd0f9428859279977172c4f2b065f08e15ed6
slab <abdullah.altaweel2002@gmail.com>
2024-04-21 03:00:55 -0400

    added ike idle

---
+++ Assets/ike_anim.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ike_anim : MonoBehaviour
+{
+    // private NavMeshAgent agent;
+    private Animator animator;
+
+    public Transform player;
+    Vector3 previousPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        update_anim_params();
+
+
+        // agent.SetDestination(player.transform.position);
+
+        float distance_from_player = Vector3.Distance(transform.position, player.transform.position);
+
+        // if (distance_from_player < agent.stoppingDistance)
+        // {
+        //     transform.LookAt(player.transform);
+        // }
+    }
+
+    private void update_anim_params()
+    {
+        // Calculate velocity
+        Vector3 currentPosition = transform.position;
+        Vector3 velocity = (currentPosition - previousPosition) / Time.deltaTime;
+
+        // Store current position as previous position for the next frame
+        previousPosition = currentPosition;
+
+        // Get the forward vector of the GameObject
+        Vector3 forwardVector = transform.forward;
+
+        // Define an arbitrary vector pointing in a different direction
+        Vector3 arbitraryVector = Vector3.up; // You can use any vector that is not collinear with the forward vector
+
+        // Calculate a vector perpendicular to the forward vector
+        Vector3 prep_vec = Vector3.Cross(forwardVector, arbitraryVector);
+
+
+        float velx = Vector3.Dot(prep_vec, velocity);
+        float vely = Vector3.Dot(transform.forward, velocity);
+        animator.SetFloat("voly", vely);
+        animator.SetFloat("volx", velx);
+    }
+}
