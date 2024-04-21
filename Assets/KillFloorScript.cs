c957e7e9523f719475316b86b8db580c3f7ebcc7
Himanshu Vora <himanshuvora24@gmail.com>
2024-04-21 18:18:19 -0400

    Fixed kill floor and bonfire bug. Added pickup weapons, breakable wall.

--- Assets/KillFloorScript.cs
+++ Assets/KillFloorScript.cs
@@ -17,7 +17,7 @@ public class KillFloorScript : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
